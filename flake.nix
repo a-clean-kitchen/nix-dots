4d1c29736c6f2240f9abb43dc1fd55ea0e0f48c9
Quade Mashaw <qmashaw@add123.com>
2022-11-29 13:10:51 -0500

    Less Initial of a commit

--- flake.nix
+++ flake.nix
@@ -1,51 +1,11 @@
 {
-  description = "Personal Nix Configuration";
-  inputs =                                                                  # All flake references used to build my NixOS setup. These are dependencies.
-    {
-      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";                  # Nix Packages
+  description = "A very basic flake";
 
-      home-manager = {                                                      # User Package Management
-        url = "github:nix-community/home-manager";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
+  outputs = { self, nixpkgs }: {
 
-      nur = {
-        url = "github:nix-community/NUR";                                   # NUR Packages
-      };
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
-      nixgl = {                                                             # OpenGL
-        url = "github:guibou/nixGL";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
 
-      emacs-overlay = {                                                     # Emacs Overlays
-        url = "github:nix-community/emacs-overlay";
-        flake = false;
-      };
-
-      doom-emacs = {                                                        # Nix-community Doom Emacs
-         url = "github:nix-community/nix-doom-emacs";
-         inputs.nixpkgs.follows = "nixpkgs";
-         inputs.emacs-overlay.follows = "emacs-overlay";
-      };
-
-      hyprland = {                                                          # Official Hyprland flake
-        url = "github:vaxerski/Hyprland";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
-    };
-
-  outputs = inputs @ { self, nixpkgs, home-manager, nur, nixgl, doom-emacs, hyprland, ... }:   # Function that tells my flake which to use and what do what to do with the dependencies.
-    let                                                                     # Variables that can be used in the config files.
-      user = "qm";
-      location = "$HOME/.setup";
-    in                                                                      # Use above variables in ...
-    {
-      nixosConfigurations = (                                               # NixOS configurations
-        import ./hosts {                                                    # Imports ./hosts/default.nix
-          inherit (nixpkgs) lib;
-          inherit inputs nixpkgs home-manager nur user location doom-emacs hyprland;   # Also inherit home-manager so it does not need to be defined here.
-        }
-      );
   };
 }
