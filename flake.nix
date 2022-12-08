9f7781c3f6181220037f9e5ed35d22ad49c9bc7f
a-clean-kitchen <mashaw.quade@gmail.com>
2022-12-08 17:51:45 -0500

    lookin usable

--- flake.nix
+++ flake.nix
@@ -1,11 +1,25 @@
 {
   description = "A very basic flake";
+  
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  outputs = { self, nixpkgs }: 
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      lib = nixpkgs.lib;
+    in {
+      nixosConfigurations = {
+        qmbocks = lib.nixosSystem {
+          inherit system;
+          modules = [ ./hosts/qmbocks/configuration.nix ];
+        };
+      };
   };
 }
