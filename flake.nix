c1ecc336a54d7470bc89e1939c77a1a7c6d3a74b
a-clean-kitchen <Mashaw.Quade@gmail.com>
2022-12-12 10:47:09 -0500

    laptop configuration

--- flake.nix
+++ flake.nix
@@ -3,10 +3,14 @@
   
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    # nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: 
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager }: 
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
