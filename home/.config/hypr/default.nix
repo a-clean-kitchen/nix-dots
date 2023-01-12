6444ff79833efd9b04370c564a7751baa461191e
a-clean-kitchen <Mashaw.Quade@gmail.com>
2023-01-12 14:08:28 -0500

    a messed up world we live in

--- home/.config/hypr/default.nix
+++ home/.config/hypr/default.nix
@@ -6,32 +6,7 @@
   };
 
   xdg.configFile."hypr/autostart" = {
-    text = ''
-      #! /usr/bin/env nix-shell
-      #! nix-shell -i bash
-
-      # variables
-      config=~/.config/hypr
-      scripts=$config/scripts
-
-      # notification daemon
-      dunst &
-
-      # waybar
-      $scripts/launch_waybar &
-      $scripts/tools/dynamic &
-
-      # wallpaper
-      $scripts/wall $config/wallpapers/cloud.png &
-
-      # effects
-      # Keeping this down until I turn rgb-rs into a flake
-      $scripts/pre-built/rgb &
-
-      # other
-      /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-      dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP &
-    '';
+    source = ./autostart;
     executable = true;
   };
 
