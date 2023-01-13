b15fa6f1967a661440d5c8e8a386c34843c09b31
a-clean-kitchen <mashaw.quade@gmail.com>
2023-01-13 01:18:54 -0500

    smal change

--- hosts/lapeaux/configuration.nix
+++ hosts/lapeaux/configuration.nix
@@ -27,7 +27,7 @@
     };
   };
   networking.hostName = "lapeaux";
-
+  networking.wireless.enable = true;
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
