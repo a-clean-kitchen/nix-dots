378c54e89ce3e646fbd03d6f6aeaf4242136e542
a-clean-kitchen <mashaw.quade@gmail.com>
2023-01-13 18:52:13 -0500

    iWd. emphasis on the W.

--- hosts/lapeaux/configuration.nix
+++ hosts/lapeaux/configuration.nix
@@ -27,7 +27,7 @@
     };
   };
   networking.hostName = "lapeaux";
-  networking.wireless.enable = true;
+  networking.wireless.iwd.enable = true;
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
