9a46ccbb9633dadd93f58a448b159398d1513672
Quade Mashaw <50602948+a-clean-kitchen@users.noreply.github.com>
2023-01-16 09:54:21 -0500

    maybe not...

--- home/default.nix
+++ home/default.nix
@@ -1,11 +1,11 @@
 { config, system, lib, pkgs, user, ... }:
-let
-  waybar = pkgs.waybar.overrideAttrs (o: {
-      unpackPhase = o.patchPhase ++ ''
-        sed -i 's/zext_workspace_handle_v1_activate(workspace_handle_);/const std::string command = "hyprctl dispatch workspace " + name_;\n\tsystem(command.c_str());/g' src/modules/wlr/workspace_manager.cpp
-      '';
-  });
-in
+# let
+#   waybar = pkgs.waybar.overrideAttrs (o: {
+#       unpackPhase = o.patchPhase ++ ''
+#         sed -i 's/zext_workspace_handle_v1_activate(workspace_handle_);/const std::string command = "hyprctl dispatch workspace " + name_;\n\tsystem(command.c_str());/g' src/modules/wlr/workspace_manager.cpp
+#       '';
+#   });
+# in
 {
   imports = (import ./.config);
 
