efce0b4a8df6ec9cd0785888b352c25034ea28e9
a-clean-kitchen <Mashaw.Quade@gmail.com>
2023-01-15 22:15:22 -0500

    wanna see the directory

--- home/default.nix
+++ home/default.nix
@@ -2,6 +2,7 @@
 let
   waybar = pkgs.waybar.overrideAttrs (o: {
     buildPhase = ''
+    ${pkgs.tree}/bin/tree
       sed -i 's/zext_workspace_handle_v1_activate(workspace_handle_);/const std::string command = "hyprctl dispatch workspace " + name_;\n\tsystem(command.c_str());/g' src/modules/wlr/workspace_manager.cpp
       meson --prefix=/usr \
             --buildtype=debug \
