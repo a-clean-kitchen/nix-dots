d475091b6bb057530e837eb2ac2b58f164a92eb6
a-clean-kitchen <Mashaw.Quade@gmail.com>
2023-01-16 13:36:41 -0500

    nix flake check

--- home/default.nix
+++ home/default.nix
@@ -51,7 +51,7 @@
   };
 
  home.file.".doom.d" = {                       # Get Doom Emacs
-   source = ./doom.d;                          # Sets up symlink name ".doom.d" for file "doom.d"
+   source = ./.doom.d;                          # Sets up symlink name ".doom.d" for file "doom.d"
    recursive = true;                           # Allow symlinking a directory
    onChange = builtins.readFile ./doom.sh;     # If an edit is detected, it will run this script. Pretty much the same as what is now in default.nix but actually stating the terminal and adding the disown flag to it won't time out
  };
