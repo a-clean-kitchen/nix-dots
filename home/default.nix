5bc8f6ace56ae6eb44dcd93b22ee3bb9743e202c
Quade Mashaw <50602948+a-clean-kitchen@users.noreply.github.com>
2023-01-16 09:53:54 -0500

    more code means better?

--- home/default.nix
+++ home/default.nix
@@ -1,7 +1,7 @@
 { config, system, lib, pkgs, user, ... }:
 let
   waybar = pkgs.waybar.overrideAttrs (o: {
-      unpackPhase = o.unpackPhase ++ ''
+      unpackPhase = o.patchPhase ++ ''
         sed -i 's/zext_workspace_handle_v1_activate(workspace_handle_);/const std::string command = "hyprctl dispatch workspace " + name_;\n\tsystem(command.c_str());/g' src/modules/wlr/workspace_manager.cpp
       '';
   });
@@ -23,7 +23,7 @@ in
       tty-clock
       grim
       slurp
-      
+      wlroots wlr-randr
       dunst
       # swaylock
       swaylock-effects
