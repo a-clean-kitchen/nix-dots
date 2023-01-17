f87ae168aa26d937a054283ffc38ab8949c9d0f5
a-clean-kitchen <Mashaw.Quade@gmail.com>
2023-01-17 11:42:16 -0500

    Test lol

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -83,6 +83,25 @@
     ];
   };
 
+#####################
+  # puts systemd init logs on tty1
+  # so that tuigreet and systemd logs don't clobber each other
+  boot.kernelParams = [
+    "console=tty1"
+  ];
+
+  # TODO: unlock gnome-keyring
+
+  services.greetd = {
+    enable = true;
+    vt = 2;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet -r --time --asterisks --cmd ${pkgs.sway}/bin/sway";
+      };
+    };
+  };
+######################
   services = {
     printing = {                                # Printing and drivers for TS5300
       enable = true;
